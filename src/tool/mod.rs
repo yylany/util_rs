a5a5e5125eae76e63427b7b360f8e405820e6089
yao <1906600192@qq.com>
2023-12-13 13:31:49 +0800

    添加 base trim

--- src/tool/mod.rs
+++ src/tool/mod.rs
@@ -4,3 +4,14 @@ pub mod libtime;
 pub mod random;
 pub mod tg;
 pub mod typ;
+
+/// base 别名移除
+pub fn base_trim(base: &str) -> &str {
+    let ts = base.trim_start_matches("1000").trim_start_matches('0');
+
+    if ts.ends_with("000") {
+        ts.trim_end_matches('0').trim_end_matches('1')
+    } else {
+        ts
+    }
+}
