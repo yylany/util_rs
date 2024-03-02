56c24f96aa9be36de9b1cecdce652ff9a0f16951
yao <1906600192@qq.com>
2024-03-02 18:16:18 +0800

    aes 密钥改造 支持 32

--- src/tool/aes.rs
+++ src/tool/aes.rs
@@ -1,5 +1,5 @@
 use aes::cipher::{generic_array::GenericArray, BlockDecrypt};
-use aes::{Aes128, NewBlockCipher};
+use aes::{Aes128, Aes256, NewBlockCipher};
 use anyhow::{anyhow, Result};
 
 fn decrypt_aes_128_ecb(ciphertext: &[u8], key: &[u8]) -> Result<Vec<u8>> {
@@ -21,6 +21,28 @@ fn decrypt_aes_128_ecb(ciphertext: &[u8], key: &[u8]) -> Result<Vec<u8>> {
     Ok(buffer)
 }
 
+fn decrypt_aes_256_ecb(ciphertext: &[u8], key: &[u8]) -> Result<Vec<u8>, anyhow::Error> {
+    if key.len() != 32 {
+        return Err(anyhow::anyhow!("密钥长度必须为 256 位（32 字节）"));
+    }
+    if ciphertext.len() % 16 != 0 {
+        return Err(anyhow::anyhow!("密文长度必须是 16 的倍数"));
+    }
+
+    // 创建一个 256 位的密钥
+    let key = GenericArray::from_slice(key);
+
+    // 初始化 AES-256-ECB cipher
+    let cipher = Aes256::new(key);
+
+    // 解密数据
+    let decrypted_data = cipher
+        .decrypt_vec(ciphertext)
+        .map_err(|e| anyhow::anyhow!(e))?;
+
+    Ok(decrypted_data)
+}
+
 // aes 128 解密
 pub fn aes_16_ecb(plaintext: &str, key: &[u8; 16]) -> Result<String> {
     use data_encoding::BASE64;
@@ -30,6 +52,21 @@ pub fn aes_16_ecb(plaintext: &str, key: &[u8; 16]) -> Result<String> {
         .map_err(|err| anyhow!("加密文件解码失败：{}", err))?;
 
     let d = String::from_utf8(decrypt_aes_128_ecb(&plaintext_data, key)?)?
+        .trim()
+        .to_string();
+
+    Ok(d)
+}
+
+// aes 128 解密
+pub fn aes_32_ecb(plaintext: &str, key: &[u8; 32]) -> Result<String> {
+    use data_encoding::BASE64;
+
+    let plaintext_data = BASE64
+        .decode(plaintext.as_bytes())
+        .map_err(|err| anyhow!("加密文件解码失败：{}", err))?;
+
+    let d = String::from_utf8(decrypt_aes_256_ecb(&plaintext_data, key)?)?
         .trim()
         .to_string();
 
