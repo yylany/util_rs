c699888ddef199a1a4198a1793aecaedd7ba6547
yao <1906600192@qq.com>
2024-09-29 14:25:16 +0800

    调整通用结构体

--- src/client/price_ceiling.rs
+++ src/client/price_ceiling.rs
@@ -1,12 +1,12 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Debug, Deserialize, Serialize, Clone)]
 pub struct PriceCeiling {
     pub coin: String,
     // 最大可用溢价
     pub max_price: f64,
     // 结束时间；单位ms
     pub end_ts: i64,
-    // 触发时间
+    // 触发时间 ;ms
     pub target: i64,
 }
