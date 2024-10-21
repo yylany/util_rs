2c70bf4d07af2336c208f6d77cc2b807d28a759c
yao <1906600192@qq.com>
2024-10-22 00:40:53 +0800

    调整 trim 函数

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,17 @@ pub fn add(left: usize, right: usize) -> usize {
 
 #[cfg(test)]
 mod tests {
-
     #[test]
-    fn it_works() {}
+    fn it_works() {
+
+        // 1000XXXUSDT，10000XXXUSDT，1000000XXXUSDT 1MXXXUSDT
+
+        assert_eq!(super::tool::base_trim("1000XXXUSDT"), "XXXUSDT");
+        assert_eq!(super::tool::base_trim("10000XXXUSDT"), "XXXUSDT");
+        assert_eq!(super::tool::base_trim("1000000XXXUSDT"), "XXXUSDT");
+        assert_eq!(super::tool::base_trim("1MXXXUSDT"), "XXXUSDT");
+        assert_eq!(super::tool::base_trim("1M10000XXXUSDT"), "XXXUSDT");
+        assert_eq!(super::tool::base_trim("1M0000XXXUSDT"), "XXXUSDT");
+        assert_eq!(super::tool::base_trim("1M0000XXXUSDT10000"), "XXXUSDT");
+    }
 }
--- src/tool/mod.rs
+++ src/tool/mod.rs
@@ -14,7 +14,7 @@ pub mod openssl_aes;
 
 /// base 别名移除
 pub fn base_trim(base: &str) -> &str {
-    let ts = base.trim_start_matches("1000").trim_start_matches('0');
+    let ts = base.trim_start_matches("1M").trim_start_matches("1000").trim_start_matches('0');
 
     if ts.ends_with("000") {
         ts.trim_end_matches('0').trim_end_matches('1')
